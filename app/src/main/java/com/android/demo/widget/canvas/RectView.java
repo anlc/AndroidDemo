44fdc168dd88bb1ab194e5f1fe271d019f08141a
v_anlaochou <v_anlaochou@baidu.com>
2019-10-02 11:19:01 +0800

    rect view

---
+++ app/src/main/java/com/android/demo/widget/canvas/RectView.java
@@ -0,0 +1,57 @@
+package com.android.demo.widget.canvas;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.demo.utils.factory.PaintFactory;
+
+/**
+ * <p>
+ *
+ * @author v_anlaochou
+ * @date 2019-09-30
+ */
+public class RectView extends View {
+
+    private Paint srcPaint;
+    private Paint dcsPaint;
+    private Paint intersectPaint;
+    private Paint unionPaint;
+    private Rect src;
+    private Rect dcs;
+    private Rect intersectSrc;
+    private Rect unionSrc;
+
+    public RectView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        src = new Rect(0, 0, 100, 100);
+        dcs = new Rect(50, 50, 200, 200);
+        intersectSrc = new Rect(src);
+        unionSrc = new Rect(src);
+
+        srcPaint = PaintFactory.createFillPaint(Color.BLUE);
+        dcsPaint = PaintFactory.createFillPaint(Color.GRAY);
+        intersectPaint = PaintFactory.createFillPaint(Color.RED);
+        unionPaint = PaintFactory.createFillPaint(Color.GREEN);
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        unionSrc.union(dcs);
+        canvas.drawRect(unionSrc, unionPaint);
+        canvas.drawRect(src, srcPaint);
+        canvas.drawRect(dcs, dcsPaint);
+        intersectSrc.intersect(dcs);
+        canvas.drawRect(intersectSrc, intersectPaint);
+    }
+}
--- app/src/test/java/com/android/demo/ExampleUnitTest.java
+++ app/src/test/java/com/android/demo/ExampleUnitTest.java
@@ -1,8 +1,13 @@
 package com.android.demo;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +16,56 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void additionIsCorrect() throws Exception {
+
+        Gson gson = new Gson();
+        List<Bean> list = new ArrayList<>();
+        list.add(new Bean("123"));
+        Result<Bean> src = new Result<>(list);
+        String json = gson.toJson(src);
+        System.out.println(json);
+
+
+        Result<Bean> result = parseTest(json, Bean.class);
+        System.out.println(result.data.get(0).name);
+    }
+
+    public <T> Result<T> parseTest(String json, Class<T> cls){
+        Result<T> result = new Result<>();
+        List<T> list = new ArrayList<>();
+        result.data = list;
+
+        Gson gson = new Gson();
+        Result<JsonElement> parseResult = gson.fromJson(json, new TypeToken<Result<JsonElement>>() {
+        }.getType());
+
+        List<JsonElement> elementList = parseResult.data;
+        for(JsonElement element : elementList){
+            T t = gson.fromJson(element.toString(), cls);
+            list.add(t);
+        }
+        return result;
+    }
+
+    static class Result<T>{
+        List<T> data;
+
+        public Result(List<T> data) {
+            this.data = data;
+        }
+
+        public Result() {
+        }
+    }
+
+    static class Bean{
+        String name;
+
+        public Bean(String name) {
+            this.name = name;
+        }
+
+        public Bean() {
+        }
     }
 }
