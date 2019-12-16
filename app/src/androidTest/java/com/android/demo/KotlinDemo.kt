600f9ef7088dd6e676b73154e631b94e5456e77e
anlc <494243786@qq.com>
2019-12-16 20:04:35 +0800

    add gradient view

---
+++ app/src/androidTest/java/com/android/demo/KotlinDemo.kt
@@ -0,0 +1,32 @@
+package com.android.demo
+
+import java.io.Serializable
+
+/**
+ * <p>
+ *
+ * @author v_anlaochou
+ * @date 2019-12-16
+ */
+
+class KLazilySingleton private constructor() : Serializable {
+
+    companion object {
+        private var mInstance: KLazilySingleton? = null
+            get() {
+                return field ?: KLazilySingleton()
+            }
+
+        fun getInstance(): KLazilySingleton {
+            return requireNotNull(mInstance)
+        }
+    }
+
+    private fun readResolve(): Any {
+        return getInstance()
+    }
+}
+
+fun main() {
+    KLazilySingleton.getInstance()
+}
--- app/src/main/java/com/android/demo/widget/canvas/DrawLineView2.kt
+++ app/src/main/java/com/android/demo/widget/canvas/DrawLineView2.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -15,20 +16,17 @@ import com.android.demo.utils.factory.PaintFactory
  * @author v_anlaochou
  * @date 2019-12-16
  */
-
-class DrawLineView : View {
+class DrawLineView2 : View {
 
     private val paint = PaintFactory.createStrokePaint(Color.BLACK)
+    private var prevX = 0f
+    private var prevY = 0f
+    private val path = Path()
     private var bitmap: Bitmap? = null
     private var bitmapCanvas: Canvas? = null
 
-    private var prevX = 0f
-    private var prevY = 0f
-    private var curX = 0f
-    private var curY = 0f
-
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-        paint.strokeWidth = 5f
+        paint.strokeWidth = 3f
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -40,23 +38,27 @@ class DrawLineView : View {
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawBitmap(bitmap, 0f, 0f, paint)
+        canvas.drawPath(path, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                prevX = event.x
-                prevY = event.y
+                path.reset()
+                prevX = x
+                prevY = y
+                path.moveTo(x, y)
             }
             MotionEvent.ACTION_MOVE -> {
-                curX = event.x
-                curY = event.y
-                bitmapCanvas?.drawLine(prevX, prevY, curX, curY, paint)
+                path.quadTo(prevX, prevY, x, y)
                 invalidate()
-                prevX = curX
-                prevY = curY
+                prevX = x
+                prevY = y
             }
             MotionEvent.ACTION_UP -> {
+                bitmapCanvas?.drawPath(path, paint)
                 invalidate()
             }
         }
