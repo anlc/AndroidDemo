fba00f0667e5da6a7db8de8fedeadfeae4bad79f
anlc <494243786@qq.com>
2019-12-19 11:10:09 +0800

    add lyric view karaoke function

---
+++ app/src/main/java/com/android/demo/activity/animation/LyricViewActivity.kt
@@ -0,0 +1,26 @@
+package com.android.demo.activity.animation
+
+import android.os.Bundle
+import com.android.demo.R
+import com.android.demo.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_lyric_view.*
+import java.util.*
+
+/**
+ * <p>
+ *
+ * @author v_anlaochou
+ * @date 2019-12-18
+ */
+class LyricViewActivity : BaseActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lyric_view)
+
+        Timer().schedule(object :TimerTask(){
+            override fun run() {
+                lyricView.postInvalidate()
+            }
+        }, 200, 200)
+    }
+}
