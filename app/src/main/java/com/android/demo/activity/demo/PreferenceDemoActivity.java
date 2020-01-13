aa9d0781ff6c9dd353fd2c8020fb2741f889be20
anlc <494243786@qq.com>
2020-01-13 15:03:37 +0800

    add preference demo

---
+++ app/src/main/java/com/android/demo/activity/demo/PreferenceDemoActivity.java
@@ -0,0 +1,34 @@
+package com.android.demo.activity.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.android.demo.R;
+
+/**
+ * <p>
+ *
+ * @author v_anlaochou
+ * @date 2020-01-13
+ */
+public class PreferenceDemoActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference_demo);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment, new GeneralPreferenceFragment())
+                .commit();
+    }
+
+    public static class GeneralPreferenceFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.fragment_prefernce_demo);
+        }
+    }
+}
