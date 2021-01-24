21137ec06d6b3cd824233bfa53134ea81c5e95fa
v_anlaochou <v_anlaochou@baidu.com>
2021-01-24 16:32:11 +0800

    feat: add intent test

---
+++ app/src/main/java/com/android/demo/activity/demo/Intent2Activity.kt
@@ -0,0 +1,28 @@
+package com.android.demo.activity.demo
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * <p>
+ *
+ * @author v_anlaochou
+ * @date 2021/1/19
+ */
+class Intent2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(TextView(this).also { it.text = "目标页面" })
+
+        val stringExtra = intent.getStringExtra("intent_data")
+        Log.d(Companion.TAG, "onCreate: $stringExtra)
+    }
+
+    companion object {
+        const val TAG = "Intent2Activity"
+    }
+}
---
+++ app/src/main/java/com/android/demo/activity/jetpack/NavigationActivity.kt
@@ -0,0 +1,17 @@
+package com.android.demo.activity.jetpack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * <p>
+ *
+ * @author v_anlaochou
+ * @date 2021/1/24
+ */
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
